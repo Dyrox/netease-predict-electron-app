97793cd2db0ee811e973b00abd1aab2b8315f05b
Dyrox <dyhmusicstuff@gmail.com>
2023-07-08 23:14:28 +0800

    new loading screen added
    有一说一只有一个arm64 mac版的，到时候其他版本都可以加上

--- src/index.js
+++ src/index.js
@@ -1,47 +1,64 @@
-// Modules to control application life and create native browser window
-const { app, BrowserWindow } = require('electron')
-const path = require('path')
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
 
-function createWindow () {
-  // Create the browser window.
-  const mainWindow = new BrowserWindow({
-    icon: __dirname + 'blueNetEase-Music-logo.icns',
-    width: 1280,
-    height: 720,
-    icon: __dirname + 'blueNetEase-Music-logo.icns',
-    webPreferences: {
+let mainWindow;
+let loadingWindow;
 
-      preload: path.join(__dirname, 'preload.js')
-    }
-  })
+function createLoadingWindow() {
+  loadingWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    frame: false, // Remove window frame for a custom loading screen
+    show: false, // Hide the window initially
+  });
 
-  // and load the index.html of the app.
-  mainWindow.loadFile('src/index.html')
+  loadingWindow.loadFile('src/loading-screen.html');
 
-
-  // Open the DevTools.
-  // mainWindow.webContents.openDevTools()
+  // Once the loading screen is ready to show, display it
+  loadingWindow.once('ready-to-show', () => {
+    loadingWindow.show();
+  });
 }
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
+function createMainWindow() {
+  mainWindow = new BrowserWindow({
+    width: 1280,
+    height: 720,
+    icon: path.join(__dirname, 'src','blueNetEase-Music-logo.icns'),
+    
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js')
+    }
+  });
+
+  mainWindow.loadFile('src/index.html');
+
+  // Open the DevTools if needed
+  // mainWindow.webContents.openDevTools();
+
+  mainWindow.once('ready-to-show', () => {
+    // Once the main window is ready to show, close the loading window
+    if (loadingWindow) {
+      loadingWindow.close();
+    }
+    mainWindow.show();
+  });
+}
+
 app.whenReady().then(() => {
-  createWindow()
+  createLoadingWindow();
+  createMainWindow();
 
   app.on('activate', function () {
-    // On macOS it's common to re-create a window in the app when the
-    // dock icon is clicked and there are no other windows open.
-    if (BrowserWindow.getAllWindows().length === 0) createWindow()
-  })
-})
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createLoadingWindow();
+      createMainWindow();
+    }
+  });
+});
 
-// Quit when all windows are closed, except on macOS. There, it's common
-// for applications and their menu bar to stay active until the user quits
-// explicitly with Cmd + Q.
 app.on('window-all-closed', function () {
-  if (process.platform !== 'darwin') app.quit()
-})
-
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
